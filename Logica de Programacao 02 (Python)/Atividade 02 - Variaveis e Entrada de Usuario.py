98c93f62c13a74ac921a9b565851aa0dec27d513
Victor Hugo <victorgugliermetti@gmail.com>
2024-08-30 17:48:17 -0300

    Semana 02

--- Logica de Programacao 02 (Python)/Atividade 02 - Variaveis e Entrada de Usuario.py
+++ Logica de Programacao 02 (Python)/Atividade 02 - Variaveis e Entrada de Usuario.py
@@ -1,3 +1,4 @@
 filme = input("Qual é o seu filme favorito? ") # Pergunta o filme favorito
 ano = input("Em que ano ele foi lançado? ") # Pergunta o ano de lançamento
 print("Meu filme favorito é " + filme + " e ele foi lançado em " + ano + " .") # Imprime a mensagem
+
